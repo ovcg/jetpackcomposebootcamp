7c9d4faa527a632fbe1494b55ccfccb315387212
otavio <17763111+ovcg@users.noreply.github.com>
2024-05-06 12:25:49 -0300

    refactor: optimize imports

--- portfolioapp/app/src/main/java/com/ovcg/portfolioapp/MainActivity.kt
+++ portfolioapp/app/src/main/java/com/ovcg/portfolioapp/MainActivity.kt
@@ -19,10 +19,8 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
